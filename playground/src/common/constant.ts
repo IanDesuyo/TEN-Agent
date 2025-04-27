990e4e123f0d348d21e81c9777c7c3af33212a16
IanDesuyo <59338745+IanDesuyo@users.noreply.github.com>
2025-04-27 11:46:32 +0800

    Force isEditModeOn = true

--- playground/src/common/constant.ts
+++ playground/src/common/constant.ts
@@ -131,7 +131,7 @@ export const isLLM = (extensionName: string) => {
   return extensionName === "llm" || extensionName === "v2v";
 }
 
-export const isEditModeOn = process.env.NEXT_PUBLIC_EDIT_GRAPH_MODE === "true";
+export const isEditModeOn = true;
 
 export const TRULIENCE_SETTINGS_KEY = "__trulience__";
 export const DEFAULT_TRULIENCE_OPTIONS: ITrulienceSettings = {
